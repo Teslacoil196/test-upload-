f0dc138cf081447cbbf2219fa2a56704c11b5dd3
Teslacoil196 <chinmaymule196@gmail.com>
2022-08-18 03:25:47 -0700

    First upload has math and calculator class

---
+++ src/basic/Calculator.java
@@ -0,0 +1,16 @@
+package basic;
+
+public class Calculator {
+	public static void main (String [] args) {
+		System.out.println("Addition");
+		
+		Math.add(22, 42);
+		
+		math.add(22,33,11);
+		
+		System.out.println("Mulriplition");
+		
+		Math.multiply(23, 2);
+		
+	}
+}
---
+++ src/basic/math.java
@@ -0,0 +1,13 @@
+package basic;
+
+public class math {
+	public static add (int a , int b) {
+		System.out.println("Sum is :"+ a+b);
+	}
+	public static multiply (int a , int b) {
+		System.out.println("Multiplication is :"+ a*b);
+	}
+	public static add (int a , int b, int c ) {
+		System.out.println("Sum is :"+ a+b+c);
+	}
+}
